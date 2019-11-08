645975646af5d517a443ab590f71dc8a45a63a3c
Lacree Cross <lacree21willams@yahoo.com>
2019-11-08 15:26:37 -0500

    completed first component

--- client/src/components/Moviecomponent.js
+++ client/src/components/Moviecomponent.js
@@ -1,2 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+class Movie extends Component {
+    state = {
+        name: '',
+        genre: '',
+        releaseYear: '',
+    }
+    render() {
+        return (<div>
+            <p>{this.state.name}</p>
+            <p>{this.state.genre}</p>
+            <p>{this.state.releaseYear}</p>
+        </div>)
+    }
+
+}
 
