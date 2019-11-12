9205a1b1ec1a34ea20a3f603392d43118b78b247
Lacree Cross <lacree21willams@yahoo.com>
2019-11-12 00:09:30 -0500

    Playing with styling about to install a npm style pakage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,18 +8,23 @@ export default class Header extends Component {
                 <button>
                     <Link to="/movies"> Movies</Link>
                 </button>
+                
                 <button>
                     <Link to="/locations"> Locations</Link>
                 </button>
+
                 <button>
                     <Link to="/reviews"> Reviews</Link>
                 </button>
+
                 <button>
                     <Link to="/movie/new"> Create New Movie</Link>
                 </button>
+
                 <button>
                     <Link to="/location/new"> Create New Location</Link>
                 </button>
+
                 <button>
                     <Link to="/review/new"> Create New Review</Link>
                 </button>
